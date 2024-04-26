bceb80db075fc8c1f9ab83645878768f2fd3a647
betrand1999 <132092468+Betrand1999@users.noreply.github.com>
2024-04-26 02:50:02 -0400

    plab

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-04e5276ebb8451442"
+    instance_type = "t3.micro"
+  
+}
+terraform {
+  backend "remote" {
+    organization = "my-organization-rich"
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
+}
