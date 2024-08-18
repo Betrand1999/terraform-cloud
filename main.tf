6de737fbcd2520b6fd350c5ab7e3d1ce6e7f08fa
betrand1999 <132092468+Betrand1999@users.noreply.github.com>
2024-08-18 12:16:28 -0400

    deploy vm

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 resource "aws_instance" "vma" {
     ami = "ami-0ae8f15ae66fe8cda"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
+    tags = {
+      Name="dev"
+    }
 }
 ######3
