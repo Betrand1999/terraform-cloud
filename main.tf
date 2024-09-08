ff33ae93c6ea670ccce9c6d70fea7cadbb33097d
BETRAND MUTAGHA <betrandmutagha@MacBook-Pro.attlocal.net>
2024-09-08 17:27:22 -0400

    adding main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,24 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "vm_a" {
-    instance_type = "t2.micro"
-    ami = "ami-066784287e358dad1"
-count= 2
-    tags = {
-      Name="dev"
-    }
+terraform { 
+  cloud { 
+    
+    organization = "my-terraform-cloud-organization" 
+
+    workspaces { 
+      name = "my-workspace" 
+    } 
+  } 
 }
-###########
+
+# resource "aws_instance" "vm_a" {
+#     instance_type = "t2.micro"
+#     ami = "ami-066784287e358dad1"
+#     tags = {
+#       Name="dev"
+#     }
+# }
+output "vm_info" {
+  value =aws_instance.vm_a.arn
+}
