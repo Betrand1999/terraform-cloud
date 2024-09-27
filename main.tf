5c4e0f86a8377b6f422a2b8c92ef4027b537a9f2
BETRAND <mmutagha@gmail.com>
2024-09-27 15:14:45 -0400

    adding a vm

--- main.tf
+++ main.tf
@@ -1,27 +1,32 @@
 provider "aws" {
-  region = "us-east-1"
-  
+  region = "us-east-2"
+
 }
 
-# terraform { 
-#   cloud { 
-    
-#     organization = "my-terraform-cloud-organization" 
 
-#     workspaces { 
-#       name = "my-workspace" 
-#     } 
-#   } 
-# }
+terraform {
+  cloud {
+
+    organization = "my-terraform-cloud-organization"
+
+    workspaces {
+      name = "my-workspace"
+    }
+  }
+}
+
+
+
+
 
 resource "aws_instance" "vm_a" {
-    instance_type = "t2.micro"
-    ami = "ami-066784287e358dad1"
-    tags = {
-      Name="Prod"
-    }
+  instance_type = "t2.micro"
+  ami           = "ami-066784287e358dad1"
+  tags = {
+    Name = "Prod"
+  }
 }
 output "vm_info" {
-  value =aws_instance.vm_a.arn
+  value = aws_instance.vm_a.arn
 }
 ###
