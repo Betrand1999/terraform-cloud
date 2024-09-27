9e7c7d1a44ca6caff998ac94cb82b0a1e9b06a6c
BETRAND <mmutagha@gmail.com>
2024-09-27 15:21:12 -0400

    adding a vm

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ terraform {
 
 
 resource "aws_instance" "vm_a" {
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   ami           = "ami-066784287e358dad1"
   tags = {
     Name = "Prod"
