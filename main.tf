60506b3d3f838996ce10a0542303d8bd1761847e
mutagha <mutagha@gmail.com>
2024-11-18 23:18:36 -0500

    adding

--- main.tf
+++ main.tf
@@ -11,13 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  cloud {
+resource "aws_instance" "vm_1" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
 
-    organization = "my-terraform-cloud-organization"
-
-    workspaces {
-      name = "my-workspace"
-    }
-  }
 }
