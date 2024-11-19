3d940b0eb555750280f4e266f222008e5fa111dd
mutagha <mutagha@gmail.com>
2024-11-18 23:34:25 -0500

    chnage name and vm type

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "vm_1" {
+resource "aws_instance" "vm_2" {
   ami           = "ami-012967cc5a8c9f891"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
 }
